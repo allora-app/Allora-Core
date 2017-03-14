9eea54fcd7be5c08dcba0e563b08ed0ad6192a17
Igor Blinnikov <Igor.Blinnikov@gmail.com>
2017-03-14 22:26:10 +0300

    fix(firebase): exports for words and courses

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,5 @@
 import * as words from './words';
-// import * as courses from './courses';
+import * as courses from './courses';
 
 export { default as init } from './initialisation';
-export * as courses from './courses';
 export { words as words };
